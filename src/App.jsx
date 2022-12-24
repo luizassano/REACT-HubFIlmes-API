654d8bd7ae83c807d50a8059ec9de8220d7d50c7
Luiz Assano <luizaugusto1974@hotmail.com>
2022-12-24 13:40:09 -0300

    feature/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
-
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="app-container">
-      <Navbar />
-      <Outlet />
-      <Navbar />
-
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <nav id="footer">
+      <h2 >
+      Copyright © Luiz Augusto Assano - 2022
+      </h2>
+    </nav>
+  );
+};
+
+export default Footer;
