5606dc12ee584f302b0b454ea9c2ef33e08cc849
Luiz Assano <luizaugusto1974@hotmail.com>
2022-12-24 13:53:18 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
-import "./App.css";
 import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
